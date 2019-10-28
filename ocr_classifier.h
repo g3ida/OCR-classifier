477dde132c2c85f34d1d97d91b8f16c72aacabc9
unknown <med.gaida@gmail.com>
2019-10-28 16:37:10 +0100

    bug fixes and refactoring

--- ocr_classifier.h
+++ ocr_classifier.h
@@ -18,21 +18,13 @@ public :
 
 	Ocr_classifier(std::string_view lang, int num_threads = 1) : lang_(lang) {
 		
-		api_ = new tesseract::TessBaseAPI();
-		if (api_->Init(NULL, lang.data())) {
-			fprintf(stderr, "Could not initialize tesseract.\n");
-			throw "could not inititialize tesseract";
-		}
-
-		if (num_threads > 1) {
-			for (int i = 0; i < num_threads-1; i++) {
-				auto api = new tesseract::TessBaseAPI();
-				if (api->Init(NULL, lang.data())) {
-					fprintf(stderr, "Could not initialize tesseract.\n");
-					throw "could not inititialize tesseract";
-				}
-				sub_api_.emplace_back(std::move(api));
+		for (int i = 0; i < num_threads; i++) {
+			auto api = new tesseract::TessBaseAPI();
+			if (api->Init(NULL, lang.data())) {
+				fprintf(stderr, "Could not initialize tesseract.\n");
+				throw "could not inititialize tesseract";
 			}
+			sub_api_.emplace_back(std::move(api));
 		}
 	}
 	
@@ -45,13 +37,7 @@ public :
 
 	std::string classifiy(PIX* image) {
 
-		std::string text;
-		if (sub_api_.size() > 0) {
-			text = apply_line_ocr_(image);
-		}
-		else {
-			text = apply_full_ocr_(image);
-		}
+		std::string text = apply_ocr_(image);
 
 		for (int i = 0; i < classes_.size(); i++) {
 			int ocrs = 0;
@@ -71,49 +57,26 @@ public :
 
 
 	~Ocr_classifier() {
-		delete api_;
+		for (auto api : sub_api_) {
+			api->End();
+			delete api;	
+		}
 	}
 private :
 
-	std::string apply_full_ocr_(PIX* image) {
+	std::string apply_ocr_(PIX* image) {
 		auto start = std::chrono::high_resolution_clock::now();
-		
-		api_->SetImage(image);
-		char* outText = api_->GetUTF8Text();
-		std::string s(outText);
-		delete[] outText;
 
-		auto stop = std::chrono::high_resolution_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-		std::cout << "ocr time : "<< duration.count() << std::endl;
-		
-		return s;
-	}
-
-	std::string apply_line_ocr_(PIX* image) {
-		auto start = std::chrono::high_resolution_clock::now();
-		
 		auto max_boxes_h = image->h / 300ULL;
-		auto boxes_h = std::min(max_boxes_h, image->h / (sub_api_.size() + 1));
+		auto boxes_h = std::min(max_boxes_h, sub_api_.size());
 
 		auto step = image->h / boxes_h;
 		auto padding = 75;
-		BOX* region = boxCreate(0, 0, image->w, step + padding);
-		PIX* imgCrop = pixClipRectangle(image, region, NULL);
-		api_->SetImage(imgCrop);
-		
-		std::vector<std::future<std::string>> futures;
 
-		futures.emplace_back(std::async(std::launch::async,
-			[&]() {
-			char* text = api_->GetUTF8Text();
-			std::string result(text);
-			delete[] text;
-			return result;
-		}));
+		std::vector<std::future<std::string>> futures;
 		
-		for (int i = 0; i < sub_api_.size(); i++) {
-			BOX* region = boxCreate(0, (i + 1) * step, image->w, (i + 2) * step + padding);
+		for (int i = 0; i < boxes_h; i++) {
+			BOX* region = boxCreate(0, (i) * step, image->w, (i + 1) * step + padding);
 			auto api = sub_api_[i];
 			futures.emplace_back(std::async(std::launch::async,
 				[=]() {
@@ -145,5 +108,4 @@ private :
 	std::vector<std::string> classes_;
 	std::vector<std::vector<std::string>> class_words_;
 	std::vector<int> occurences_;
-	tesseract::TessBaseAPI* api_ = nullptr;
 };
